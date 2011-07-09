1990b79c13529c2c7486aaffe2cee433f10cf414
John Wang <john.wang@gmail.com>
2011-07-09 15:41:59 -0700

    hooked up api to django

--- app/content_store/views.py
+++ app/content_store/views.py
@@ -1,17 +1,27 @@
 # Create your views here.
 from django.http import HttpResponse
-        
+from utils import json
+
 def newIndex(request,index_name):
-	return HttpResponse("index %s created" %index_name)
+	resp = {'store':index_name,'sensei-host':'localhost','sensei-port':8080,'sensei-path':'sensei'}
+	return HttpResponse(json.json_encode(resp))
 
 def getSize(request,index_name):
-	return HttpResponse("size %d" % 0)
+	resp = {'store':index_name,"size":0}
+	return HttpResponse(json.json_encode(resp))
 	
-def getDoc(request,id):
-	return HttpResponse("uid: %d" % int(id))
+def getDoc(request,index_name,id):
+	uid = long(id)
+	doc = {'id':uid}
+	resp = {'store':index_name,'doc':doc}
+	return HttpResponse(json.json_encode(resp))
 
-def addDoc(request,id):
-	return HttpResponse("uid: %d" % int(id))
+def addDoc(request,index_name,id):
+	uid = long(id)
+	doc = {'id':uid}
+	resp = {'store':index_name,'doc':doc}
+	return HttpResponse(json.json_encode(resp))
 
 def available(request,index_name):
-	return HttpResponse("index %s created" %index_name)
+	resp = {'store':index_name,"available":True}
+	return HttpResponse(json.json_encode(resp))
