021fdd0c9730ce1608eb3dca37ac8375ad91669f
Lei Wang <liwang@linkedin.com>
2011-07-14 11:37:50 -0700

    redirect to index automatically.

--- app/urls.py
+++ app/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls.defaults import patterns, include, url
 # from django.contrib import admin
 # admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns('views',
   # Examples:
   # url(r'^$', 'sinApp.views.home', name='home'),
   # url(r'^sinApp/', include('sinApp.foo.urls')),
@@ -14,6 +14,9 @@ urlpatterns = patterns('',
 
   # Uncomment the next line to enable the admin:
   # url(r'^admin/', include(admin.site.urls)),
+
+  url(r'^$', 'home', name='home'),
+
   url(r'^cluster/', include('cluster.urls')),
   url(r'^store/', include('content_store.urls')),
 )
---
+++ app/views.py
@@ -0,0 +1,10 @@
+from django.conf import settings
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+from django.http import Http404
+
+from utils import enum, json
+
+def home(request):
+  return HttpResponseRedirect('/static/index.html')
+
