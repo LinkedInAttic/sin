08d5b1b3c503c4c17dafd9eaaf2eb4a151436f09
Lei Wang <liwang@linkedin.com>
2011-07-14 10:22:43 -0700

    change a little of the url mapping.

--- app/cluster/urls.py
+++ app/cluster/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('cluster.views',
-  (r'^(?P<group_id>[^/]+)/?$','nodes'),
+  (r'^(?P<group_id>[^/]+)/nodes/?$','nodes'),
   (r'^(?P<group_id>[^/]+)/nodes/count/?$','nodes_count'),
 )
