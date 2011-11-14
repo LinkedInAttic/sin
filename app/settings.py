35dda8efa355f14c38c2012ccee90089f9c222d2
Lei Wang <liwang@linkedin.com>
2011-11-14 15:36:10 -0800

    restore db location.

--- app/settings.py
+++ app/settings.py
@@ -23,7 +23,7 @@ MANAGERS = ADMINS
 DATABASES = {
   'default': {
     'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-    'NAME': '/var/sin/sin.db',            # Or path to database file if using sqlite3.
+    'NAME': '/tmp/sin',            # Or path to database file if using sqlite3.
     'USER': '',            # Not used with sqlite3.
     'PASSWORD': '',          # Not used with sqlite3.
     'HOST': '',            # Set to empty string for localhost. Not used with sqlite3.
